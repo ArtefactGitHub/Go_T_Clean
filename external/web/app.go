1d319c4effed848cb7857edbb9f2808975c6503d
ArtefactGitHub <artefactgo@gmail.com>
2022-03-24 18:02:47 +0900

    Appの抽象化

--- external/web/app.go
+++ external/web/app.go
@@ -6,22 +6,32 @@ import (
 	"net/http"
 
 	"github.com/ArtefactGitHub/Go_T_Clean/domain/interactor"
+	"github.com/ArtefactGitHub/Go_T_Clean/external/common"
 	"github.com/ArtefactGitHub/Go_T_Clean/external/web/controller"
 	"github.com/ArtefactGitHub/Go_T_Clean/external/web/middleware"
 	"github.com/ArtefactGitHub/Go_T_Clean/external/web/model"
 	"github.com/ArtefactGitHub/Go_T_Clean/external/web/route"
 	"github.com/ArtefactGitHub/Go_T_Clean/infurastructure"
+	ifmodel "github.com/ArtefactGitHub/Go_T_Clean/infurastructure/model"
 	"github.com/gorilla/mux"
 )
 
-type App struct{}
+type webApp struct {
+	deployType common.DeployType
+	storeType  common.StoreType
+}
 
-func (app *App) Run() {
+func NewWebApp(deployType common.DeployType, storeType common.StoreType) common.App {
+	app := webApp{deployType: deployType, storeType: storeType}
+	return &app
+}
+
+func (app *webApp) Run() error {
 	url := "localhost"
 	port := "8080"
 
 	router := mux.NewRouter()
-	routes := getRoutes()
+	routes := app.getRoutes()
 	for _, r := range routes {
 		switch r.Method {
 		case http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete:
@@ -34,12 +44,27 @@ func (app *App) Run() {
 
 	address := fmt.Sprintf("%s:%s", url, port)
 	log.Printf("running on %s", address)
-	log.Fatal(http.ListenAndServe(address, middleware.MethodOverride(router)))
+	return http.ListenAndServe(address, middleware.MethodOverride(router))
 }
 
-func getRoutes() []model.Route {
-	repository := infurastructure.NewInMemoryTaskRepository()
-	interactor := interactor.NewTaskInteractor(repository)
+func (app *webApp) getRoutes() []model.Route {
+	if app.storeType.IsMySql() {
+		repository, err := infurastructure.NewMySqlTaskRepository(ifmodel.NewMySqlSetting(
+			"", "", "", "", "", "",
+		))
+		if err != nil {
+			log.Fatalf("NewMySqlTaskRepository() error: %s", err.Error())
+		}
 
-	return route.NewTaskRoute(interactor).GetRoutes()
+		interactor := interactor.NewTaskInteractor(repository)
+		return route.NewTaskRoute(interactor).GetRoutes()
+	} else {
+		repository, err := infurastructure.NewInMemoryTaskRepository()
+		if err != nil {
+			log.Fatalf("NewInMemoryTaskRepository() error: %s", err.Error())
+		}
+
+		interactor := interactor.NewTaskInteractor(repository)
+		return route.NewTaskRoute(interactor).GetRoutes()
+	}
 }
